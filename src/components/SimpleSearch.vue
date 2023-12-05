6af9d3423d9c53dca6c8570d7700862c03f0e743
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-12-05 02:58:40 +0100

    Milestone: 0

---
+++ src/components/SimpleSearch.vue
@@ -0,0 +1,28 @@
+<script>
+import { store } from "../store.js"
+
+export default {
+  name: "SearchBar",
+
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    search() {
+      this.$emit("search")
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="store.searchText" @keyup.enter="$emit('search')">
+    <button @click="search">cerca</button>
+
+  </div>
+</template>
+
+<style></style>
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+
+    myApiKey: "f37b3c0c8eae36fd294a8da28899a911",
+    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=",
+
+
+
+});
+
+
+
+
+
+
+
+// f37b3c0c8eae36fd294a8da28899a911
